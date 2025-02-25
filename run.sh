1f2fd2d2cff97ec518e96fe7cd814588eaf09dea
Alex <format37@gmail.com>
2025-02-25 16:50:24 +0400

    GPU id updated to 0th 	modified:   run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 docker rm -f groundingdino_container 2>/dev/null || true
-docker run --gpus '"device=2"' -it --rm \
+docker run --gpus '"device=1"' -it --rm \
   --name groundingdino_container \
   -v "$(pwd)/output:/app/GroundingDINO/output" \
   groundingdino_image
